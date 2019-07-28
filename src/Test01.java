56acc45e4bf8422951da676df5282cf03098f030
shit <1209332243@qq.com>
2019-07-28 21:47:21 +0800

    提交测试1

---
+++ src/Test01.java
@@ -0,0 +1,10 @@
+/**
+ * @author dell
+ * @version v 1.0
+ * @date 2019/7/28
+ */
+public class Test01 {
+    public static void main(String[] args) {
+        System.out.println("dfdsdsdsdsdsdvdvdvd");
+    }
+}
